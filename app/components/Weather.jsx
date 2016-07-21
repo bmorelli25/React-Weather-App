8679a7e318879ff5f87cd3842881b3c047411df2
pentel13 <bmorelli25@gmail.com>
2016-07-21 11:14:45 -0700

    openweathermap.jsx added: API wired up
    weather.jsx, weatherform.jsx, and weathermessage.jsx all interact to
    pull data from openweathermap

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
   getInitialState: function () {
@@ -11,10 +12,15 @@ var Weather = React.createClass({
     }
   },
   handleSearch: function (location) { //function that is called by WeatherForm.jsx's 'this.props.onSearch'
-    //faking our call for now.
-    this.setState({
-      location: location,
-      temp: 23
+    var that = this;
+
+    openWeatherMap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp
+      });
+    }, function (errorMessage) {
+      alert(errorMessage);
     });
   },
   render: function () {
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
       WeatherForm: 'app/components/WeatherForm.jsx',
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       About: 'app/components/About.jsx',
-      Examples: 'app/components/Examples.jsx'
+      Examples: 'app/components/Examples.jsx',
+      openWeatherMap: 'app/api/openWeatherMap.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
