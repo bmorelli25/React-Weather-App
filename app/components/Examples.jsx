ceac46c9d458c8474c4a2ba896b402e439b3020c
pentel13 <bmorelli25@gmail.com>
2016-07-25 14:22:38 -0700

    styled main.jsx and examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to='/?location=Portland'>Portland, OR</Link>
+        </li>
+        <li>
+          <Link to='/?location=London'>London, UK</Link>
+        </li>
+      </ol>
     </div>
   );
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h2>Main component</h2>
-      {props.children}
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 };
