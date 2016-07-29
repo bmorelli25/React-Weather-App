0fd1893f91a079e0c2fdacc344b306cf1c0201d6
pentel13 <bmorelli25@gmail.com>
2016-07-29 11:17:14 -0700

    removed unnecessary files for gh-pages

--- app/components/WeatherMessage.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var WeatherMessage = ({temp, location}) => {  //get our props from Weather.jsx with ES6 destructuring
-  return (
-      <h3 className="text-center">It is {temp}&deg; in {location}</h3>
-  );
-}
-
-module.exports = WeatherMessage;
