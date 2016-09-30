10710ca878c9852f9c5dd1f633eadae3a33607b7
avi-jain <avijain97@ymail.com>
2016-10-01 05:18:12 +0530

    Running...

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,14 +1,20 @@
 var React = require('react');
 
 var WeatherMessage = React.createClass({
+	//Have hard coded a few messages.
+	//In future, the tips for each category can be chosen from an array of similar messages. 
+	randomizeTip:function(){
+	  var index = Math.floor(Math.random() * tips.length);
+	  return tips[index];
+	},
 	generateTip: function () {
     switch (this.props.weather[0].main) {
 			case "Clouds":
-			    return "Rain rain go away...";
+			    return "It's a beautiful sunny day. Just Kidding. Grab your umbrellas.";
 			case "Atmosphere":
-			    return "";
+			    return "Uhmm...";
 			case "Clear":
-			    return "Clear blue sky";
+			    return "Clear blue skies";
 			case "Extreme":
 				if(this.props.weather[0].main.id == 904)
 			    return "Dammit global warming.";
@@ -17,7 +23,7 @@ var WeatherMessage = React.createClass({
 			case "Drizzle":
 			    return "Grab a cup of coffee and read a book.";
 			case "Rain":
-			    return "It's a beautiful sunny day. Just Kiddding. It's pouring";
+			    return "It's a beautiful sunny day. Just Kiddding. It's pouring!";
 			case "Snow":
 			    return "It's ducking freezing";
 			default : return "For more details, look out the window."
