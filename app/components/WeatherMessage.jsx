7a816b9e6c12836f88dafff78da172922e773f1a
avi-jain <avijain97@ymail.com>
2016-10-01 04:10:39 +0530

    Converted WeatherMessage to React Comp

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,13 +1,18 @@
 var React = require('react');
 
 var WeatherMessage = React.createClass({
-
+	generateTip: function () {
+    if(this.props.weather[0].main == "Clouds"){
+    	return "Testing"
+    }
+  },
   render: function() {
   	var {temp, location} = this.props
+  	var tip = this.generateTip();
     return (
 	  	<div>
 	      <h3 className="text-center">It is {temp}&deg; in {location}</h3>
-	      <h3 className="text-center"></h3>
+	      <h3 className="text-center">{tip}</h3>
 	    </div>
   	);
   }
