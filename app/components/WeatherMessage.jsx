6385582c09f5340992d3f810905703f9e29e6519
avi-jain <avijain97@ymail.com>
2016-10-01 04:56:13 +0530

    Included last commit which adds tests

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,26 @@ var React = require('react');
 
 var WeatherMessage = React.createClass({
 	generateTip: function () {
-    if(this.props.weather[0].main == "Clouds"){
-    	return "Testing"
-    }
+    switch (this.props.weather[0].main) {
+			case "Clouds":
+			    return "Rain rain go away...";
+			case "Atmosphere":
+			    return "";
+			case "Clear":
+			    return "Clear blue sky";
+			case "Extreme":
+				if(this.props.weather[0].main.id == 904)
+			    return "Dammit global warming.";
+			case "Thunderstorm":
+			    return "Stay in bed.";
+			case "Drizzle":
+			    return "Grab a cup of coffee and read a book.";
+			case "Rain":
+			    return "It's a beautiful sunny day. Just Kiddding. It's pouring";
+			case "Snow":
+			    return "It's ducking freezing";
+			default : return "For more details, look out the window."
+		}
   },
   render: function() {
   	var {temp, location} = this.props
