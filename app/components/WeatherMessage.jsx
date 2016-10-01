ce9e4d22e16034083fd5df2e571cc775bd338c0f
Brandon Morelli <bmorelli25@gmail.com>
2016-10-01 10:03:29 -0700

    Refactoring WeatherMessage.jsx
    Using a map instead of switch statement

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,35 +1,28 @@
 var React = require('react');
 
 var WeatherMessage = React.createClass({
-	// Can be refactored to pick up random tips from a map/dictionary (keys -> conditions)
 	generateTip: function () {
-    switch (this.props.condition.main) {
-			case 'Clouds':
-			    return "Cloudy with a chance of meatballs";
-			case 'Atmosphere':
-			    return str.strike("Love") + "Mist is in the air.";
-			case 'Clear':
-			    return "Clear blue skies";
-			case 'Extreme':
-			    return "Dammit global warming.";
-			case 'Thunderstorm':
-			    return "Stay in bed today.";
-			case 'Drizzle':
-			    return "Grab a book and a cup of coffee";
-			case 'Rain':
-			    return "It's a beautiful sunny day. Just Kiddding. It's pouring";
-			case 'Snow':
-			    return "Colder than your dating streak";
-			default : return "For more details, look out the window."
-		}
+    var condition = String(this.props.condition.main);
+    if (condition != 'undefined') {
+      var conditionMap = {
+        'Clear': "Clear blue skies",
+        'Thunderstorm': "Stay in bed today.",
+        'Drizzle': "Grab a book and a cup of coffee",
+        'Extreme': "Dammit global warming.",
+        'Rain': "It's a beautiful sunny day. Just Kiddding. It's pouring",
+        'Snow': "Colder than your dating streak",
+        'Atmosphere': "Mist is in the air.",
+        'Clouds': "Cloudy with a chance of meatballs"
+      };
+      return conditionMap[condition] ? conditionMap[condition] : '';
+    };
   },
   render: function() {
-  	var {temp, location} = this.props
-  	var tip = this.generateTip();
+  	var {temp, location} = this.props;
     return (
 	  	<div>
 	      <h3 className="text-center">It is {temp}&deg; in {location}</h3>
-	      <h3 className="text-center">{tip}</h3>
+	      <h3 className="text-center">{this.generateTip()}</h3>
 	    </div>
   	);
   }
