9bb566e0a27f9b3498d9a7ef0c7b2d8f4fd43e6a
Kimmi G <kimmi.gan@kopijunkie.net>
2016-10-01 16:59:04 +0100

    WeatherForecast component to display 5-day forecast

---
+++ app/components/WeatherForecast.jsx
@@ -0,0 +1,108 @@
+var React = require('react');
+
+var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
+
+var WeatherForecast = React.createClass({
+
+	getInitialState: function () {
+		return {
+			isLoading: true,
+			errorMessage: null,
+			data: null
+		};
+	},
+
+	componentWillMount: function () {
+		openWeatherMap.get5DayForecast(this.props.location).then(weatherForecast => {
+			const forecastData = weatherForecast.list;
+			this.setState({
+        isLoading: false,
+        data: forecastData
+      });
+    }, e => {
+      this.setState({
+        isLoading: false,
+        errorMessage: e.message
+      });
+    });
+	},
+
+	render: function () {
+		const { isLoading, errorMessage } = this.state;
+		const hasError = !isLoading && errorMessage;
+		return (
+			<div>
+				{hasError ? <ErrorModal message={errorMessage} /> : this.renderForecast()}
+			</div>
+		);
+	},
+
+  renderForecast: function() {
+  	if (this.state.isLoading) {
+  		return <h3 className="text-center">Fetching 5-Day Forecast...</h3>;
+  	}
+
+  	return (
+			<div>
+				<h4 className="forecast-title">Forecast for next 5 days</h4>
+				<DailyWeatherList data={this.state.data} />
+			</div>
+		);
+  }
+
+});
+
+var DailyWeatherList = React.createClass({
+
+	render: function () {
+		const fiveDaySummary = this.getFiveDaySummary(this.props.data);
+		console.log("fiveDaySummary:",fiveDaySummary);
+		return (
+			<div className="row">
+				{fiveDaySummary.map(this.renderWeatherSummary)}
+			</div>
+		);
+	},
+
+	renderWeatherSummary: function (weatherData, index) {
+		const date = this.getDate(weatherData.dt_txt);
+		const temp = weatherData.main.temp;
+		const weather = weatherData.weather[0];
+		return (
+			<div key={index} className="small-6 column">
+				<div className="forecast-container">
+					{date}
+					<br />
+					Temp: {weatherData.main.temp}&deg;
+					<br />
+					{weather.main}
+				</div>
+			</div>);
+	},
+
+	getFiveDaySummary: function (fullData) {
+		// get an array of unique dates from the returned API data
+		const dates = [... new Set(fullData.map(data => this.getDate(data.dt_txt)))];
+
+		const fiveDaySummary = [...dates].map((date => {
+			const threeHourlyWeatherForDay = fullData.filter(data => {
+				if (date === this.getDate(data.dt_txt)) {
+					return data.weather;
+				}
+			});
+			// TODO?: think about what time's weather to return as the day's forecasted weather
+			return threeHourlyWeatherForDay[0];
+		}))
+		fiveDaySummary.splice(0, 1);	// API returns 6 days's data (i.e. includes today)
+		return fiveDaySummary;
+	},
+
+	getDate: function (dateTimeStr) {
+		const dateTime = dateTimeStr.split(" ");
+		return dateTime[0];
+	}
+
+});
+
+module.exports = WeatherForecast;
