9ca92af40011dcc8439be78aacfb9f332d681121
pentel13 <bmorelli25@gmail.com>
2016-10-06 09:39:11 -0700

    Removing Aliases for testing purposes

--- app/tests/sanity.spec.js
+++ app/tests/sanity.spec.js
@@ -1,4 +1,5 @@
 var assert = require('chai').assert;
+var Nav = require('../components/Nav.jsx');
 
 describe('Sanity Check', function () {
 	it('should be able to run mocha and use chai for assertions', function () {
