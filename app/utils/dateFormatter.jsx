bmorelli25/React-Weather-App

commit da1273d3e5255b55c92cba8ba12d89b0a0349860

Replaced dateFormatter.getDayDateLabel function
Changed to getDayLabel and getDateLabel to allow for greater formatting
control on the output

app/utils/dateFormatter.jsx
@@ -9,9 +9,14 @@ module.exports = {
 		return date;
 	},
 
-	getDayDateLabel: function (dateTimeString) {
+	getDayLabel: function (dateTimeString) {
 		const date = this.getDate(dateTimeString);
-		return date.format('dddd, MMM D');
+		return date.format('dddd');
+	},
+
+	getDateLabel: function (dateTimeString) {
+		const date = this.getDate(dateTimeString);
+		return date.format('MMM D');
 	}
 
 };

app/components/WeatherForecast.jsx
@@ -70,19 +70,20 @@ var DailyWeatherList = React.createClass({
 		const temp = weatherData.main.temp;
 		const weather = weatherData.weather[0];
 		return (
-			<div key={index} className="row">
-				<div className="small-10 small-centered column">
-					<div className="forecast-container">
-						<h4>{index === 0 ? 'Tomorrow' : dateFormatter.getDayDateLabel(dateTimeStr)}</h4>
-						<div className="row collapse">
-							<div className="small-2 medium-3 column weather-icon">
-								<i className={`wi wi-owm-${weather.id}`} />
-							</div>
-							<div className="small-10 medium-9 column">
-								{weatherData.main.temp}&deg;
-								<br />
-								{weather.description}
-							</div>
+			<div key={index} className="day">
+				<div className="">
+					<h4>
+						<span className="dayLabel">{index === 0 ? 'Tomorrow' : dateFormatter.getDayLabel(dateTimeStr) + ', '}</span>
+						<span className="dateLabel">{index === 0 ? '\u00a0' : dateFormatter.getDateLabel(dateTimeStr)}</span>
+					</h4>
+					<div className="row collapse">
+						<div className="weather-icon">
+							<i className={`wi wi-owm-${weather.id}`} />
+						</div>
+						<div className="">
+							{weatherData.main.temp}&deg;
+							<br />
+							{weather.description}
 						</div>
 					</div>
 				</div>

commit 2ce925557f49c25c683f94500a1749c61428a4a5

Re-added dateFormatter.getDayDateLabel
...to prevent breaking changes introduced in da1273d

app/utils/dateFormatter.jsx
@@ -9,6 +9,11 @@ module.exports = {
 		return date;
 	},
 
+	getDayDateLabel: function (dateTimeString) {
+		const date = this.getDate(dateTimeString);
+		return date.format('dddd MMM D');
+	},
+
 	getDayLabel: function (dateTimeString) {
 		const date = this.getDate(dateTimeString);
 		return date.format('dddd');