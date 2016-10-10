0a18348bd028feb93069c008192855cf0d24f0d8
pentel13 <bmorelli25@gmail.com>
2016-10-10 11:28:34 -0700

    Refactoring WeatherForecast.jsx

--- app/components/WeatherForecast.jsx
+++ app/components/WeatherForecast.jsx
@@ -1,12 +1,43 @@
-const React = require('react');
+var React = require('react');
 
-const dateFormatter = require('../utils/dateFormatter.jsx');
-const openWeatherMap = require('../api/openWeatherMap.jsx');
-const ErrorModal = require('./ErrorModal.jsx');
+var dateFormatter = require('../utils/dateFormatter.jsx');
+var openWeatherMap = require('../api/openWeatherMap.jsx');
+var ErrorModal = require('./ErrorModal.jsx');
 
-const WeatherForecast = React.createClass({
+// var WeatherForecast = React.createClass({
+//
+//
+//
+// 	render: function () {
+// 		const { isLoading, errorMessage } = this.state;
+// 		const hasError = !isLoading && errorMessage;
+// 		return (
+// 			<div>
+// 				{hasError ? <ErrorModal message={errorMessage} /> : this.renderForecast()}
+// 			</div>
+// 		);
+// 	},
+//
+//   renderForecast: function() {
+//   	if (this.state.isLoading) {
+//   		return <h3 className="text-center">Fetching 5-Day Forecast...</h3>;
+//   	}
+//
+//   	return (
+// 			<div>
+// 				<h4 className="forecast-title">Forecast for next 5 days</h4>
+// 				<DailyWeatherList data={this.state.data} />
+// 			</div>
+// 		);
+//   }
+//
+// });
+
+//var DailyWeatherList = React.createClass({
+var WeatherForecast = React.createClass({
 
 	getInitialState: function () {
+		console.log("1");
 		return {
 			isLoading: true,
 			errorMessage: null,
@@ -15,11 +46,11 @@ const WeatherForecast = React.createClass({
 	},
 
 	componentWillMount: function () {
+		console.log("componentWillMount");
 		openWeatherMap.getWeather('forecast', this.props.location).then(weatherForecast => {
-			const forecastData = weatherForecast.list;
 			this.setState({
         isLoading: false,
-        data: forecastData
+        data: weatherForecast.list
       });
     }, e => {
       this.setState({
@@ -29,46 +60,12 @@ const WeatherForecast = React.createClass({
     });
 	},
 
-	render: function () {
-		const { isLoading, errorMessage } = this.state;
-		const hasError = !isLoading && errorMessage;
-		return (
-			<div>
-				{hasError ? <ErrorModal message={errorMessage} /> : this.renderForecast()}
-			</div>
-		);
-	},
-
-  renderForecast: function() {
-  	if (this.state.isLoading) {
-  		return <h3 className="text-center">Fetching 5-Day Forecast...</h3>;
-  	}
-
-  	return (
-			<div>
-				<h4 className="forecast-title">Forecast for next 5 days</h4>
-				<DailyWeatherList data={this.state.data} />
-			</div>
-		);
-  }
-
-});
-
-var DailyWeatherList = React.createClass({
-
-	render: function () {
-		const fiveDaySummary = this.getFiveDaySummary(this.props.data);
-		return (
-			<div>
-				{fiveDaySummary.map(this.renderWeatherSummary)}
-			</div>
-		);
-	},
-
 	renderWeatherSummary: function (weatherData, index) {
-		const dateTimeStr = weatherData.dt_txt;
-		const temp = weatherData.main.temp;
-		const weather = weatherData.weather[0];
+		console.log("3");
+		{this.renderError()}
+		const dateTimeStr = this.state.data.dt_txt;
+		const temp = this.state.data.main.temp;
+		const weather = this.state.data.weather[0];
 		return (
 			<div key={index} className="row">
 				<div className="small-10 small-centered column">
@@ -79,22 +76,24 @@ var DailyWeatherList = React.createClass({
 								<i className={`wi wi-owm-${weather.id}`} />
 							</div>
 							<div className="small-10 medium-9 column">
-								{weatherData.main.temp}&deg;
+								{this.state.data.main.temp}&deg;
 								<br />
 								{weather.description}
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>);
+			</div>
+		);
 	},
 
-	getFiveDaySummary: function (fullData) {
+	getFiveDaySummary: function () {
+		console.log("4");
 		// get an array of unique date strings from the returned API data
-		const dates = [... new Set(fullData.map(data => this.getDateStr(data.dt_txt)))];
+		const dates = [... new Set(this.state.data.map(data => this.getDateStr(data.dt_txt)))];
 
 		const fiveDaySummary = [...dates].map((date => {
-			const threeHourlyWeatherForDay = fullData.filter(data => {
+			const threeHourlyWeatherForDay = this.state.data.filter(data => {
 				if (date === this.getDateStr(data.dt_txt)) {
 					return data.weather;
 				}
@@ -103,14 +102,56 @@ var DailyWeatherList = React.createClass({
 			return threeHourlyWeatherForDay[0];
 		}))
 		fiveDaySummary.splice(0, 1);	// API returns 6 days's data (i.e. includes today)
+		console.log("Five Day Summary: ");
+		console.log(fiveDaySummary);
 		return fiveDaySummary;
 	},
 
 	getDateStr: function (dateTimeStr) {
+		//called from getFiveDaySummary()
+		console.log("5");
 		const dateTime = dateTimeStr.split(" ");
 		return dateTime[0];
-	}
+	},
 
+	renderForecast: function() {
+		console.log("2");
+  	if (this.state.isLoading) {
+  		return <h3 className="text-center">Fetching 5-Day Forecast...</h3>;
+  	}
+
+		const fiveDaySummary = this.getFiveDaySummary();
+  	return (
+			<div>
+				<h4 className="forecast-title">Forecast for next 5 days</h4>
+				{this.getFiveDaySummary.map(this.renderWeatherSummary)}
+			</div>
+		);
+  },
+
+	renderError: function () {
+		if (typeof errorMessage === 'string'){
+			return (
+				<ErrorModal message={errorMessage}/>
+			)
+		};
+	},
+
+	render: function () {
+		//const fiveDaySummary = this.getFiveDaySummary();
+
+		return (
+			<div>
+				<p>Hello</p>
+				{this.renderForecast()}
+				{this.renderError()}
+			</div>
+		);
+	}
 });
 
 module.exports = WeatherForecast;
+
+// {this.renderForecast()}
+// {fiveDaySummary.map(this.renderWeatherSummary)}
+// {renderError()}
