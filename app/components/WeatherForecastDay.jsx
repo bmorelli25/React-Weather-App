07eac0bbc1dff49be78ed56e3973b43c7ac66df6
pentel13 <bmorelli25@gmail.com>
2016-10-10 12:06:53 -0700

    Removing unnecessary code

--- app/components/WeatherForecastDay.jsx
+++ app/components/WeatherForecastDay.jsx
@@ -8,35 +8,34 @@ var WeatherForecastDay = React.createClass({
 
 	render: function () {
 		console.log(this.props);
-		//const fiveDaySummary = this.getFiveDaySummary();
-		// console.log("3");
-		//
-		// const dateTimeStr = this.props.dt_txt;
-		// const temp = this.props.main.temp;
-		// const weather = this.props.weather[0];
-		//
-		// return (
-		// 	<div key={index} className="row">
-		// 		<div className="small-10 small-centered column">
-		// 			<div className="forecast-container">
-		// 				<h4>{index === 0 ? 'Tomorrow' : dateFormatter.getDayDateLabel(dateTimeStr)}</h4>
-		// 				<div className="row collapse">
-		// 					<div className="small-2 medium-3 column weather-icon">
-		// 						<i className={`wi wi-owm-${weather.id}`} />
-		// 					</div>
-		// 					<div className="small-10 medium-9 column">
-		// 						{this.props.data.main.temp}&deg;
-		// 						<br />
-		// 						{weather.description}
-		// 					</div>
-		// 				</div>
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// );
+		// const fiveDaySummary = this.getFiveDaySummary();
+
+		const dateTimeStr = this.props.dt;
+		const temp = this.props.main.temp;
+		const weather = this.props.weather[0];
+
 		return (
-			<h1>Weather Forecast Day</h1>
+			<div className="row">
+				<div className="small-10 small-centered column">
+					<div className="forecast-container">
+						<h4>{dateTimeStr === 0 ? 'Tomorrow' : dateTimeStr}</h4>
+						<div className="row collapse">
+							<div className="small-2 medium-3 column weather-icon">
+								<i className={`wi wi-owm-${weather.id}`} />
+							</div>
+							<div className="small-10 medium-9 column">
+								{this.props.main.temp}&deg;
+								<br />
+								{weather.description}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
+		// return (
+		// 	<h1>Weather Forecast Day</h1>
+		// );
 	}
 });
 
--- app/components/WeatherForecastList.jsx
+++ app/components/WeatherForecastList.jsx
@@ -32,33 +32,6 @@ var WeatherForecastList = React.createClass({
     });
 	},
 
-	renderWeatherSummary: function (weatherData, index) {
-		console.log("3");
-		{this.renderError()}
-		const dateTimeStr = this.state.data.dt_txt;
-		const temp = this.state.data.main.temp;
-		const weather = this.state.data.weather[0];
-		return (
-			<div key={index} className="row">
-				<div className="small-10 small-centered column">
-					<div className="forecast-container">
-						<h4>{index === 0 ? 'Tomorrow' : dateFormatter.getDayDateLabel(dateTimeStr)}</h4>
-						<div className="row collapse">
-							<div className="small-2 medium-3 column weather-icon">
-								<i className={`wi wi-owm-${weather.id}`} />
-							</div>
-							<div className="small-10 medium-9 column">
-								{this.state.data.main.temp}&deg;
-								<br />
-								{weather.description}
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	},
-
 	getFiveDaySummary: function () {
 		console.log("4");
 		// get an array of unique date strings from the returned API data
@@ -74,33 +47,15 @@ var WeatherForecastList = React.createClass({
 			return threeHourlyWeatherForDay[0];
 		}))
 		fiveDaySummary.splice(0, 1);	// API returns 6 days's data (i.e. includes today)
-		console.log("Five Day Summary: ");
-		console.log(fiveDaySummary);
 		return fiveDaySummary;
 	},
 
 	getDateStr: function (dateTimeStr) {
-		//called from getFiveDaySummary()
-		console.log("5");
 		const dateTime = dateTimeStr.split(" ");
 		return dateTime[0];
 	},
 
-	//CHECKS IF DATA HAS BEEN FETCHED. IF SO, FORMATS RETURNED DATA. CALLED BY RENDER()
-	renderForecast: function() {
-		console.log("2");
-  	if (this.state.isLoading) {
-  		return <h3 className="text-center">Fetching 5-Day Forecast...</h3>;
-  	}
 
-		const fiveDaySummary = this.getFiveDaySummary();
-  	return (
-			<div>
-				<h4 className="forecast-title">Forecast for next 5 days</h4>
-				{this.getFiveDaySummary.map(this.renderWeatherSummary)}
-			</div>
-		);
-  },
 
 	//RENDERS AN ERROR IF ENCOUNTERED. CALLED BY RENDER()
 	renderError: function () {
@@ -112,10 +67,18 @@ var WeatherForecastList = React.createClass({
 	},
 
 	render: function () {
-		//const fiveDaySummary = this.getFiveDaySummary();
 
 		var renderDays = () => {
-			return weatherArrayTemp.map((day) => {
+			//TODO remove first element from list of days
+			//TODO cap at 5 days just in case
+
+			if (this.state.isLoading) { //if data hasn't loaded yet
+				return <h3 className="text-center">Fetching 5-Day Forecast...</h3>;
+			}
+
+			//TODO Return H4 Class as well
+			//<h4 className="forecast-title">Forecast for next 5 days</h4>
+			return weatherArrayTemp.map((day) => { //once data loads
 				return <WeatherForecastDay key={day.dt} {...day}/>
 			})
 		};
