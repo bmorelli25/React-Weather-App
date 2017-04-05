c69564fb15e2223f6d8363061498a9025adf648a
Brandon Morelli <bmorelli25@gmail.com>
2017-04-05 08:52:44 -0700

    Forgot to export Foundation

--- app/styles/foundation.js
+++ app/styles/foundation.js
@@ -3,4 +3,6 @@ var $ = require('jquery');
 global.jQuery = $;
 
 // to require all features of foundation
-require('../../node_modules/foundation-sites/dist/js/foundation.js');
+require('../../node_modules/foundation-sites/dist/js/foundation.min.js');
+
+export default Foundation;
